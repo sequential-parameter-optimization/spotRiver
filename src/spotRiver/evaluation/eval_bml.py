4d580ecae8f9fae99cdefdad8d006759f6760880
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2023-02-20 19:14:45 +0100

    Update eval_bml.py

--- src/spotRiver/evaluation/eval_bml.py
+++ src/spotRiver/evaluation/eval_bml.py
@@ -10,7 +10,7 @@ from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
 
-def evaluateModel(diff, memory, time):
+def evaluate_model(diff, memory, time):
     pos_sum = 0
     neg_sum = 0
     for e in diff:
@@ -32,24 +32,6 @@ def evaluateModel(diff, memory, time):
         "CompTime (s)": round(time, 4),
     }
     return res_dict
-
-
-def plot_results(df_eval=None, Metric=False, df_true=None, real_vs_predict=False):
-    if df_eval is not None and Metric:
-        fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(16, 5), constrained_layout=True, sharex=True)
-        ax1.plot(df_eval.index, df_eval["MAE"])
-        ax1.set_title("Mean Absolute Error")
-        ax2.plot(df_eval.index, df_eval["Memory (MB)"])
-        ax2.set_title("Memory (MB)")
-        ax3.plot(df_eval.index, df_eval["CompTime (s)"])
-        ax3.set_title("Computation time (s)")
-    if df_true is not None and real_vs_predict:
-        plt.figure(figsize=(16, 5))
-        plt.plot(df_true.index, df_true["Vibration"], label="Actual")
-        plt.plot(df_true.index, df_true["Prediction"], label="Prediction")
-        plt.title("Actual vs Prediction")
-        plt.legend()
-        plt.show()
 
 
 def eval_bml(train=None, test=None, horizon=None, model=None):
@@ -89,65 +71,69 @@ def eval_bml(train=None, test=None, horizon=None, model=None):
     )
     if horizon is None:
         for i in range(0, len(test)):
-            start = datetime.now()
-            tracemalloc.start()
-            forecast = model.predict(array(test.iloc[i, :-1]).reshape(1, -1))
-            preds = Series(forecast, index=[i])
-            diffs = test.iloc[i, -1] - preds
-            current, peak = tracemalloc.get_traced_memory()
-            end = datetime.now()
-            time = (end - start).total_seconds()
-            df_eval.loc[i + 1] = Series(evaluateModel(diffs, (peak / 10**6), time))
-            series_preds = concat([series_preds, preds])
-            series_diffs = concat([series_diffs, diffs])
+            series_preds, series_diffs = eval_one(df_eval, i, model, horizon, test, series_preds, series_diffs)
     if horizon is not None:
         if len(test) % horizon == 0:
             for i in range(0, (int(len(test) / horizon) - 1)):
-                start = datetime.now()
-                tracemalloc.start()
-                forecast = model.predict(test.iloc[i * horizon : (i + 1) * horizon, :-1])
-                preds = Series(forecast)
-                diffs = test.iloc[i * horizon : (i + 1) * horizon, -1].values - preds
-                current, peak = tracemalloc.get_traced_memory()
-                end = datetime.now()
-                time = (end - start).total_seconds()
-                df_eval.loc[i + 1] = Series(evaluateModel(diffs, (peak / 10**6), time))
-                series_preds = concat([series_preds, preds])
-                series_diffs = concat([series_diffs, diffs])
+                series_preds, series_diffs = eval_one(df_eval, i, model, horizon, test, series_preds, series_diffs)
             series_preds = series_preds.reset_index(drop=True)
             series_diffs = series_diffs.reset_index(drop=True)
         if len(test) % horizon != 0:
             length = floor(len(test) / horizon)
             for i in range(0, (int(length))):
-                start = datetime.now()
-                tracemalloc.start()
-                forecast = model.predict(test.iloc[i * horizon : (i + 1) * horizon, :-1])
-                preds = Series(forecast)
-                diffs = test.iloc[i * horizon : (i + 1) * horizon, -1].values - preds
-                current, peak = tracemalloc.get_traced_memory()
-                end = datetime.now()
-                time = (end - start).total_seconds()
-                df_eval.loc[i + 1] = Series(evaluateModel(diffs, (peak / 10**6), time))
-                series_preds = concat([series_preds, preds])
-                series_diffs = concat([series_diffs, diffs])
-            start = datetime.now()
-            tracemalloc.start()
-            forecast = model.predict(
-                test.iloc[int(length * horizon) : int((length * horizon + len(test) % horizon - 1)), :-1]
-            )
-            preds = Series(forecast)
-            diffs = (
-                test.iloc[int(length * horizon) : int((length * horizon + len(test) % horizon - 1)), -1].values - preds
-            )
-            current, peak = tracemalloc.get_traced_memory()
-            end = datetime.now()
-            time = (end - start).total_seconds()
-            df_eval.loc[int(length)] = Series(evaluateModel(diffs, (peak / 10**6), time))
-            series_preds = concat([series_preds, preds])
-            series_diffs = concat([series_diffs, diffs])
+                series_preds, series_diffs = eval_one(df_eval, i, model, horizon, test, series_preds, series_diffs)
+            series_preds, series_diffs = eval_last(df_eval, length, model, horizon, test, series_preds, series_diffs)
             series_preds = series_preds.reset_index(drop=True)
             series_diffs = series_diffs.reset_index(drop=True)
     df_true = test.copy()
     df_true["Prediction"] = series_preds
     df_true["Difference"] = series_diffs
     return df_eval, df_true, series_preds, series_diffs
+
+
+def eval_one(df_eval, i, model, horizon, test, series_preds, series_diffs):
+    start = datetime.now()
+    tracemalloc.start()
+    forecast = model.predict(test.iloc[i * horizon : (i + 1) * horizon, :-1])
+    preds = Series(forecast)
+    diffs = test.iloc[i * horizon : (i + 1) * horizon, -1].values - preds
+    current, peak = tracemalloc.get_traced_memory()
+    end = datetime.now()
+    time = (end - start).total_seconds()
+    df_eval.loc[i + 1] = Series(evaluate_model(diffs, (peak / 10**6), time))
+    series_preds = concat([series_preds, preds])
+    series_diffs = concat([series_diffs, diffs])
+    return series_preds, series_diffs
+
+
+def eval_last(df_eval, length, model, horizon, test, series_preds, series_diffs):
+    start = datetime.now()
+    tracemalloc.start()
+    forecast = model.predict(test.iloc[int(length * horizon) : int((length * horizon + len(test) % horizon - 1)), :-1])
+    preds = Series(forecast)
+    diffs = test.iloc[int(length * horizon) : int((length * horizon + len(test) % horizon - 1)), -1].values - preds
+    current, peak = tracemalloc.get_traced_memory()
+    end = datetime.now()
+    time = (end - start).total_seconds()
+    df_eval.loc[int(length)] = Series(evaluate_model(diffs, (peak / 10**6), time))
+    series_preds = concat([series_preds, preds])
+    series_diffs = concat([series_diffs, diffs])
+    return series_preds, series_diffs
+
+
+def plot_results(df_eval=None, metric=False, df_true=None, real_vs_predict=False):
+    if df_eval is not None and metric:
+        fig, (ax1, ax2, ax3) = plt.subplots(3, figsize=(16, 5), constrained_layout=True, sharex=True)
+        ax1.plot(df_eval.index, df_eval["MAE"])
+        ax1.set_title("Mean Absolute Error")
+        ax2.plot(df_eval.index, df_eval["Memory (MB)"])
+        ax2.set_title("Memory (MB)")
+        ax3.plot(df_eval.index, df_eval["CompTime (s)"])
+        ax3.set_title("Computation time (s)")
+    if df_true is not None and real_vs_predict:
+        plt.figure(figsize=(16, 5))
+        plt.plot(df_true.index, df_true["Vibration"], label="Actual")
+        plt.plot(df_true.index, df_true["Prediction"], label="Prediction")
+        plt.title("Actual vs Prediction")
+        plt.legend()
+        plt.show()

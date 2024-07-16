2b74d7083aeadaca1bdb1e2503590dab63eb6abe
bartzbeielstein <32470350+bartzbeielstein@users.noreply.github.com>
2024-07-16 16:26:48 +0200

    0.3.9
    Introducing scenarios

--- src/spotRiver/hyperdict/river_hyper_dict.py
+++ src/spotRiver/hyperdict/river_hyper_dict.py
@@ -15,12 +15,16 @@ class RiverHyperDict(base.FileConfig):
         self.filename = filename
         self.directory = directory
         self.hyper_dict = self.load()
+        self.scenario = "river"
 
     @property
     def path(self):
         if self.directory:
             return pathlib.Path(self.directory).joinpath(self.filename)
         return pathlib.Path(__file__).parent.joinpath(self.filename)
+
+    def get_scenario(self):
+        return self.scenario
 
     def load(self) -> dict:
         """Load the hyperparameters from the file.
